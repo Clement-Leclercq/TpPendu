cc841a6c79a5981cf92d7dacb782404a9b609ff2
Clement <leclercqcl@yahoo.fr>
2020-12-03 12:22:50 +0100

    commit

--- main.py
+++ main.py
@@ -17,24 +17,26 @@ def startGame(): #Cette fonction lance simplement le jeu
     listeLettre = []
     test = False
     while vie > 0 and test == False:
-        print("Le mot que vous devez trouver est:")
-        print(motcache)
-        print("Il est composé de "+str(len(mot))+" lettres.")
-        print("Il vous reste "+str(vie)+" essais pour trouver le mot.")
         if motcache != mot:
+            print("Le mot que vous devez trouver est:")
+            print(motcache)
+            print("Il est composé de "+str(len(mot))+" lettres.")
+            print("Il vous reste "+str(vie)+" essais pour trouver le mot.")
             lettre = inputLetter(listeLettre)
             listeLettre.append(lettre)
             if lettre not in mot:
                 vie -= 1
                 print("Dommage la lettre "+lettre+" n'est pas dans le mot !")
+                print("******************************")
             else:
                 print("Bravo la lettre "+lettre+" était bien dans le mot !")
                 motcache = playerWord(lettre,mot,motcache)
+                print("******************************")
         else:
             test = True
     if test:
         print("Bravo vous avez trouvé le mot "+mot+" !")
-        print("Il vous reste "+str(vie)+" vies.")
+        print("Il vous restait "+str(vie)+" vies.")
     else:
         print("Dommage vous n'avez pas réussi à trouver le mot qui était:")
         print(mot)
