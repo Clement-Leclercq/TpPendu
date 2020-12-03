4e6e3b2ec73d95070b22234c443154af8df46efd
Clement <leclercqcl@yahoo.fr>
2020-12-03 11:02:24 +0100

    ok

--- fonctionTerminal.py
+++ fonctionTerminal.py
@@ -8,34 +8,17 @@ Fait le 03/12/2020
 TO DO: fichier de test complet
 """
 from random import randint #On importe le module qui nous permet de générer un nombre entier aléatoire
-from fichier import fileReading #On importe la fonction nous permettant
-
 
 def randomElement(liste): #Cette fonction prend un élément aléatoire dans une liste
     randomNumber = randint(0,len(liste)-1)
     return liste[randomNumber]
 
-def startGame(): #Cette fonction lance simplement le jeu
-    mot = randomElement(fileReading("word.txt"))
-    motcache = hiddenWord(mot)
-    vie = 8
-    test = False
-    while vie > 0 and test == False:
-        print("Le mot que vous devez trouver est:\n")
-        print(motcache)
-        print("Il est composé de "+len(mot)+" lettres.")
-        print("Il vous reste "+vie+" essais pour trouver le mot.")
-        if motcache != mot:
-            lettre = inputLetter()
-            if lettre not in mot:
-                vie -= 1
-                print("Dommage la lettre "+lettre+" n'est pas dans le mot !")
-            else:
-                print("Bravo la lettre "+lettre+" était bien dans le mot !")
-                motcache = playerWord(lettre,mot,motcache)
-        else:
-            test = True
-
+def newTry(): #Cette fonction demande à l'utilisateur s'il veut rejouer et renvoie True ou False en fonction de la réponse (Oui ou Non)
+    valRen = False
+    reponse = input("Voulez vous rejouer (Oui ou Non) ?\n")
+    if reponse[0].upper() == "O":
+        valRen = True
+    return valRen
 
 def checkInput(lettre): #Cette fonction vérifie que c'est bien une seule lettre qui a été rentrée
     if (len(lettre) == 1 and lettre in "AZERTYUIOPQSDFGHJKLMWXCVBN"):
--- main.py
+++ main.py
@@ -5,8 +5,38 @@
 Ce programme est le programme principal de notre jeu, il permet de lancer des parties
 Programme fait par Clément Leclercq
 Fait le 03/12/2020
-TO DO: tout
+TO DO: Rajouter les high scores
 """
-from fonctionTerminal import startGame #cet import va nous permettre de commencer le jeu
+from fonctionTerminal import randomElement,hiddenWord,inputLetter,newTry,playerWord
+from fichier import fileReading
+
+def startGame(): #Cette fonction lance simplement le jeu
+    mot = randomElement(fileReading("word.txt"))
+    motcache = hiddenWord(mot)
+    vie = 8
+    test = False
+    while vie > 0 and test == False:
+        print("Le mot que vous devez trouver est:")
+        print(motcache)
+        print("Il est composé de "+str(len(mot))+" lettres.")
+        print("Il vous reste "+str(vie)+" essais pour trouver le mot.")
+        if motcache != mot:
+            lettre = inputLetter()
+            if lettre not in mot:
+                vie -= 1
+                print("Dommage la lettre "+lettre+" n'est pas dans le mot !")
+            else:
+                print("Bravo la lettre "+lettre+" était bien dans le mot !")
+                motcache = playerWord(lettre,mot,motcache)
+        else:
+            test = True
+    if test:
+        print("Bravo vous avez trouvé le mot "+mot+" !")
+        print("Il vous reste "+str(vie)+" vies.")
+    else:
+        print("Dommage vous n'avez pas réussi à trouver le mot qui était:")
+        print(mot)
+    if newTry():
+        startGame()
 
 startGame()
